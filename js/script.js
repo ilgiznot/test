f4b874dc7a93ffb0228377ba5b31ce1f53015d00
Ilgiz <saitek95@mail.ru>
2021-01-15 11:44:22 +0300

    мой первый урок

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов?' ,'');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из фильмов', ''),
+      b = prompt('Оценка', ''),
+      c = prompt('Один из фильмов', ''),
+      d = prompt('Оценка', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
